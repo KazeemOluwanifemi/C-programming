29bd1eaecbb8ee8cd6d81f4b004c6216f47f2a5a
KazeemOluwanifemi <bethkaz934@gmail.com>
2024-01-03 14:24:24 +0100

    Addition operation with C

--- main.c
+++ main.c
@@ -2,6 +2,11 @@
 #include "cs50.h"
 
 int main(void) {
-   string answer = get_string("What's your name, muggle?");
-   printf("Hello, %s\n", answer);
+   // string answer = get_string("What's your name, muggle?");
+   // printf("Hello, %s\n", answer);
+
+   int x = get_int("Enter your value of x: ");
+   int y = get_int("Enter your value of y: ");
+
+   printf("%i\n", x+y);
 }
