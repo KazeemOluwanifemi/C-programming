a769876711d557fa510edb3bf7a8b3eb88042ca4
KazeemOluwanifemi <bethkaz934@gmail.com>
2024-01-05 23:05:59 +0100

    If..else conditions

--- main.c
+++ main.c
@@ -11,10 +11,25 @@ int main(void) {
    // printf("%i\n", x+y);
 
    // truncation...dividing with different data types
-   int x = get_int("Enter your x value: ");
-   int y = get_int("Enter your y value: ");
+   // int x = get_int("Enter your x value: ");
+   // int y = get_int("Enter your y value: ");
 
    // Casting: converting one data type to another
-   float z = (float)x/(float)y;
-   printf("%f\n", z);
+   // float z = (float)x/(float)y;
+   // printf("%f\n", z);
+
+   // Conditions
+   int x = get_int("Enter your value of x: ");
+   int y = get_int("Enter your value of y: ");
+
+   if(x > y){
+      printf("x is greater than y.");
+   }
+   else if(x < y){
+      printf("x is less than y.");
+   }
+   else{
+      printf("x is equal to y.");
+   }
+
 }
