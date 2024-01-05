15e64c63fa645506bbf3ceeecc1aceb9e66ab130
KazeemOluwanifemi <bethkaz934@gmail.com>
2024-01-05 22:53:53 +0100

    Truncation and Casting in C language

--- main.c
+++ main.c
@@ -5,8 +5,16 @@ int main(void) {
    // string answer = get_string("What's your name, muggle?");
    // printf("Hello, %s\n", answer);
 
-   int x = get_int("Enter your value of x: ");
-   int y = get_int("Enter your value of y: ");
+   // int x = get_int("Enter your value of x: ");
+   // int y = get_int("Enter your value of y: ");
 
-   printf("%i\n", x+y);
+   // printf("%i\n", x+y);
+
+   // truncation...dividing with different data types
+   int x = get_int("Enter your x value: ");
+   int y = get_int("Enter your y value: ");
+
+   // Casting: converting one data type to another
+   float z = (float)x/(float)y;
+   printf("%f\n", z);
 }
