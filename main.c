ce79b652e642751bbd5186f2e5c99ec901fd9eaf
KazeemOluwanifemi <bethkaz934@gmail.com>
2024-01-06 17:12:25 +0100

    Updates to If..else statements

--- main.c
+++ main.c
@@ -1,35 +1,15 @@
 #include <stdio.h>
 #include "cs50.h"
 
-int main(void) {
-   // string answer = get_string("What's your name, muggle?");
-   // printf("Hello, %s\n", answer);
-
-   // int x = get_int("Enter your value of x: ");
-   // int y = get_int("Enter your value of y: ");
-
-   // printf("%i\n", x+y);
-
-   // truncation...dividing with different data types
-   // int x = get_int("Enter your x value: ");
-   // int y = get_int("Enter your y value: ");
-
-   // Casting: converting one data type to another
-   // float z = (float)x/(float)y;
-   // printf("%f\n", z);
-
-   // Conditions
-   int x = get_int("Enter your value of x: ");
-   int y = get_int("Enter your value of y: ");
-
-   if(x > y){
-      printf("x is greater than y.");
+int main(void){
+   char answer = get_char("Do you agree? Y/N ");
+   if(answer == "Y"){
+      printf("Agreed.");
    }
-   else if(x < y){
-      printf("x is less than y.");
+   else if(answer == "N"){
+      printf("Disagreed.");
    }
    else{
-      printf("x is equal to y.");
+      printf("Input valid answer.");
    }
-
 }
