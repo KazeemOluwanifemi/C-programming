de0d9b244fc26e4894dbd29e787353dae52115bf
KazeemOluwanifemi <bethkaz934@gmail.com>
2024-01-06 20:24:42 +0100

    For Loop syntax

--- main.c
+++ main.c
@@ -5,12 +5,18 @@ int main(void){
    // char answer = get_char("Do you agree? Y/N ");
 
    // While statement
-   int i = 0;
-   while (i < 50)
+   // int i = 0;
+   // while (i < 50)
+   // {
+   //    printf("Hola weurld.\n");
+   //    i++;
+   // }
+   // // For Statement
+   for (int i = 0; i < 50; i++)
    {
-      printf("Hola weurld.\n");
-      i++;
+      printf("Hello World \n");
    }
+   
    
    // if(answer == 'Y' || answer == 'y'){
    //    printf("Agreed.");
