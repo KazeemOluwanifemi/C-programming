d512cbff6afea43f81b89406077bcebc1aeed256
KazeemOluwanifemi <bethkaz934@gmail.com>
2024-01-06 17:55:51 +0100

    Loop statement - for

--- main.c
+++ main.c
@@ -2,14 +2,23 @@
 #include "cs50.h"
 
 int main(void){
-   char answer = get_char("Do you agree? Y/N ");
-   if(answer == 'Y'){
-      printf("Agreed.");
+   // char answer = get_char("Do you agree? Y/N ");
+
+   // While statement
+   int i = 0;
+   while (i < 50)
+   {
+      printf("Hola weurld.\n");
+      i++;
    }
-   else if(answer == 'N'){
-      printf("Disagreed.");
-   }
-   else{
-      printf("Input valid answer.");
-   }
+   
+   // if(answer == 'Y' || answer == 'y'){
+   //    printf("Agreed.");
+   // }
+   // else if(answer == 'N' || answer == 'n'){
+   //    printf("Disagreed.");
+   // }
+   // else{
+   //    printf("Input valid answer.");
+   // }
 }
