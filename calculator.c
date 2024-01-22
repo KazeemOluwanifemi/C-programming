40068a8c67c7c956563eac262378ed01a0121e01
KazeemOluwanifemi <bethkaz934@gmail.com>
2024-01-22 16:49:31 +0100

    create new mini-project file

---
+++ calculator.c
@@ -0,0 +1,10 @@
+// Define the elements to be used 
+// Integers (0-9)
+// Arithmentic Operators(+,-,/,*,%)
+// Assignment Operator(=)
+// Logical Operators(==, >,<,>=,<=)
+
+// Specify the Algorithm
+// *Take input from user 
+// Perform operations based on the user's input
+// Error Handling 
