c58b190d0b168bac15b8516e7a658b89c4447aa5
KazeemOluwanifemi <bethkaz934@gmail.com>
2024-01-25 23:04:46 +0100

    started C mini-project

--- calculator.c
+++ calculator.c
@@ -1,10 +1,20 @@
 // Define the elements to be used 
-// Integers (0-9)
-// Arithmentic Operators(+,-,/,*,%)
+// Integers (0-9)-> Array
+// Arithmentic Operators(+,-,/,*,%)-> Array
 // Assignment Operator(=)
-// Logical Operators(==, >,<,>=,<=)
+// Logical Operators(==, >,<,>=,<=)-> Array
 
 // Specify the Algorithm
 // *Take input from user 
 // Perform operations based on the user's input
 // Error Handling 
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+    int numbersInput[] = {0,1,2,3,4,5,6,7,8,9};
+    printf("%d", numbersInput[4]);
+}
