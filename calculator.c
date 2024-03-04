4363bfffa4ee6bedcd3d8a1b65d3596c2dd603b8
KazeemOluwanifemi <bethkaz934@gmail.com>
2024-03-04 12:45:49 +0100

    optimized code by creating functions

--- calculator.c
+++ calculator.c
@@ -1,8 +1,7 @@
 // Define the elements to be used 
-// Integers (0-9)-> Array
-// Arithmentic Operators(+,-,/,*)-> Array
+// Integers (0-9)
+// Arithmentic Operators(+,-,/,*)
 // Assignment Operator(=)
-// Logical Operators(==, >,<,>=,<=)-> Array
 
 // Specify the Algorithm
 // *Take input from user 
@@ -16,19 +15,10 @@
 #include <stdlib.h>
 #include <math.h>
 
-int main(){
-// Get integer values from users
-    printf("%s", "Input two operands: \n");
-    int firstValue = 0;
-    int secondValue = 0;
-    scanf("%i %i", &firstValue, &secondValue);
-
-// Get operator from user
-    printf("%s", "Select an operator from the following: \n +,-,/,*,^\n");
-    char operatorVal;
-    scanf(" %c", &operatorVal);
 
 // Calculate and print based on the user's operator
+int calculator(char operatorVal, int firstValue, int secondValue)
+{
     switch(operatorVal)
     {
         case '-':
@@ -49,4 +39,20 @@ int main(){
         default:
             printf("Error!! Please enter a valid operator.");
     }
+}
+
+int main(){
+// Get integer values from users
+    printf("%s", "Input two operands: \n");
+    int firstValue = 0;
+    int secondValue = 0;
+    scanf("%i %i", &firstValue, &secondValue);
+
+// Get operator from user
+    printf("%s", "Select an operator from the following: \n +,-,/,*,^\n");
+    char operatorVal;
+    scanf(" %c", &operatorVal);
+
+// Calculate by calling the calclator function
+    int result = calculator(operatorVal, firstValue, secondValue);
 }
