d518fcf2e4e7cb3493688b24141d3bbbf99ce1f6
KazeemOluwanifemi <bethkaz934@gmail.com>
2024-03-06 21:49:06 +0100

    defined elements to be used

---
+++ SMS.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+
+// operations to be carried out by this program:
+// Add student information
+// View student information/List all student information(Display total number of students)
+// Remove/Delete student information
+// Find a Student by the matriculation number
+// Student information to be recorded
+// Categories:
+// -> Personal Info
+    // 1. Name, DOB, Age, State of Origin
+// -> Academic Info
+    // MatNo, RegNo, College of Study, CGPA, Deparment
+// -> Residential Info
+//    Hall, Room Number
+
+int addName(){};
+int removeName(){};
+int listName(){};
+
+int main(){
+    // functions
+}
