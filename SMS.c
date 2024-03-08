68e7566eb2e355470ded0a06760b71d8e2fc60a8
KazeemOluwanifemi <bethkaz934@gmail.com>
2024-03-08 11:02:12 +0100

    built fmain screeen

--- SMS.c
+++ SMS.c
@@ -18,7 +18,39 @@
 int addName(){};
 int removeName(){};
 int listName(){};
+int searchMatNO(){};
 
 int main(){
+    // Create main screen
+    printf("%s", "\t\t\t\t\t\t |----------STUDENT DATABASE MANAGEMENT SYSTEM----------| \t\t\t\t\t\t\n\n");
+    printf("%s", "\t\t\t\t\t\t\t\t 1. Add Student Data \n");
+    printf("%s", "\t\t\t\t\t\t\t\t 2. Delete Student Data\n");
+    printf("%s", "\t\t\t\t\t\t\t\t 3. List Student Data\n");
+    printf("%s", "\t\t\t\t\t\t\t\t 4. Search with Matriculation Number\n");
+    printf("%s", "\t\t\t\t\t\t\t\t 5. Exit\n");
+    printf("%s", "\t\t\t\t\t\t _______________________________________________________ \t\t\t\t\t\t\n\n");
+
+    // Ask user to choose an option
+    printf("%s", "Pick an option: ");
+    int choice;
+    scanf("%i", &choice);
+
+    // Perform operation based on user's choice
+    switch(choice){
+        case 1:
+            addName();
+            break;
+        case 2:
+            removeName();
+            break;
+        case 3:
+            listName();
+            break;
+        case 4:
+            searchMatNo();
+            break;
+        case 5:
+            exit(0);
+    }
     // functions
 }
