3dd8adf3012e11e5187bcb71b7555a7c5658902d
KazeemOluwanifemi <bethkaz934@gmail.com>
2025-07-09 09:40:11 +0100

    exam practice day 2, pointers and arrays

--- test-practice.c
+++ test-practice.c
@@ -4,11 +4,11 @@ int main(){
     int arr[] = {10,20,30,40,50,60,70,80};
     int i;
     int len = sizeof(arr)/sizeof(arr[i]);
-    int sum;
+    int sum = 0;
 
     // printf("%d", len);
 
-    for(i = 0; i <= len; i+2){
+    for(i = 0; i < len ; i+=2){
         sum = arr[i] + arr[i + 1];
     }
 
